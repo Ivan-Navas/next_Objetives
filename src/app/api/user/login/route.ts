8cb9876e7cd5ec70a0eb9b369667b1ee1468650d
Ivan-Navas <ivanrng.work@gmail.com>
2025-07-12 15:31:44 -0500

    bug: :big: fixed login api

--- src/app/api/user/login/route.ts
+++ src/app/api/user/login/route.ts
@@ -1,60 +1,63 @@
-import { NextResponse } from "next/server";
-import User from "@/interface/user";
 import { prisma } from "@/libs/prisma";
-const jwt = require("jsonwebtoken");
-import { cookies } from "next/headers";
-const cookie = require("cookie");
+import { NextRequest, NextResponse } from "next/server";
+import jwt from "jsonwebtoken";
 
-export const POST = async (req: any, res: any) => {
-  const user: User = await req.json();
-
+export const POST = async (req: NextRequest) => {
   try {
-    const exist = await prisma.user.findFirst({
+    const {email, password} = await req.json();
+    if(!email || !password){
+      return NextResponse.json({
+        status: "error",
+        message: "Faltan datos por llenar"
+      })
+    }
+    const userExist = await prisma.user.findFirst({
       where: {
-        email: user.email,
-        password: user.password,
+        email: email,
+      },
+      select: {
+        id: true,
+        name: true,
+        email: true,
+        password: true,
+      }
+    })
+    if(!userExist || password !== userExist.password){
+      return NextResponse.json({
+        status: "error",
+        message: "Correo o contraseña incorrecta"
+      })
+    }
+    const token = jwt.sign(
+      {
+        id: userExist.id,
+        name: userExist.name,
+        email: userExist.email,
+      },
+      process.env.SECRET_VALUE!,
+      {expiresIn: "30d"},
+    )
+    const response =  NextResponse.json({
+      status: "success",
+      message: "Iniciando sesíon",
+      user: {
+        id: userExist.id,
+        name: userExist.name,
+        email: userExist.email,
       },
     });
-    if (exist) {
-      const token = jwt.sign(
-        {
-          email: exist.email,
-          name: exist.name,
-          id: exist.id,
-          exp: Math.floor(Date.now() / 1000 + 60 * 60 * 24 * 30),
-        },
-        process.env.SECRET_VALUE
-      );
-
-      cookies().set({
-        name: "token",
-        value: token,
-        httpOnly: true,
-        path: "/",
-        maxAge: 1000 * 60 * 60 * 24 * 30,
-        secure: process.env.NODE_ENV === "production",
-      });
-
-      return NextResponse.json({
-        status: "success",
-        message: "Logeado con exito",
-        user: {
-          id: exist.id,
-          name: exist.name,
-          email: exist.email,
-        },
-      });
-    }
-
+    response.cookies.set("token", token, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      sameSite: "strict",
+      path: "/",
+      maxAge: 30 * 60 * 60 * 24, // 30 días
+    });
+    return response
+  } catch (error) {
     return NextResponse.json({
       status: "error",
-      message: "Correo o contraseña incorrecto",
-    });
-  } catch (error: any) {
-    return NextResponse.json({
-      status: "error",
-      message: "Ocurrio un error al iniciar sesíon",
-      error: error.message,
-    });
+      message: "Ocurrió un error",
+    })
   }
 };
