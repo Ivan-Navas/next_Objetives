79a4605b612197cc7fa6dc5e170bd00ada45b58b
Ivan-Navas <ivanrng.work@gmail.com>
2025-07-13 23:02:09 -0500

    bug: :bug: resolve api route

--- src/app/api/user/logout/route.ts
+++ src/app/api/user/logout/route.ts
@@ -1,35 +1,27 @@
-import { cookies } from "next/headers";
 import { NextRequest, NextResponse } from "next/server";
-const { verify } = require("jsonwebtoken");
+import jwt from "jsonwebtoken";
 
-export const POST = async (request: NextRequest) => {
-  const token = cookies().get("token");
-  if (!token?.value) {
+export const POST = async (req: NextRequest) => {
+  const token = req.cookies.get("token")?.value;
+  if (!token) {
     return NextResponse.json({
       status: "error",
       message: "No estas logeado",
     });
   }
-
   try {
-    verify(token.value, process.env.SECRET_VALUE);
-
-    cookies().set({
-      name: "token",
-      value: "",
-      httpOnly: true,
-      path: "/",
-      maxAge: 0,
-      secure: process.env.NODE_ENV === "production",
-    });
-    return NextResponse.json({
-      status: "succes",
+    jwt.verify(token, process.env.SECRET_VALUE!);
+    const response =  NextResponse.json({
+      status: "success",
       message: "Sesion cerrada",
     });
+    response.cookies.delete("token");
+    return response;
   } catch (error) {
     return NextResponse.json({
       status: "error",
       message: "Token invalido",
+      error,
     });
   }
 };
