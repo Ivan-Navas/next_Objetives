5a06fa8f6c7bf0648a580bacbb678a56ad7c00f9
Ivan-Navas <ivanrng.work@gmail.com>
2025-07-16 08:33:15 -0500

    bug: :bug: fixed middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,30 +1,16 @@
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import { jwtVerify } from "jose";
 
-export const middleware = async (request: any) => {
-  const token = request.cookies.get("token");
+export const middleware = async (req: NextRequest) => {
+  const token = req.cookies.get("token")?.value;
+  if (token === undefined) {
+    return NextResponse.redirect(new URL("/login", req.url));
+  }
+};
 
-  if (request.nextUrl.pathname.includes("/page/feed")) {
-    if (token === undefined) {
-      return NextResponse.redirect(new URL("/page/login", request.url));
-    }
-    try {
-      const ob = await jwtVerify(
-        token.value,
-        new TextEncoder().encode(process.env.SECRET_VALUE)
-      );
-    } catch (error) {
-      console.log(error);
-      return NextResponse.redirect(new URL("/page/login", request.url));
-    }
-    return NextResponse.next();
-  }
-  if (
-    request.nextUrl.pathname.includes("/page/login") ||
-    request.nextUrl.pathname.includes("/page/register")
-  ) {
-    if (token !== undefined) {
-      return NextResponse.redirect(new URL("/page/feed", request.url));
-    }
-  }
+export const config = {
+  matcher: [
+    "/",
+    "/feed",
+  ],
 };
