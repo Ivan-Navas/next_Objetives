fce027af3ee21e176228bf1dfbcbd54183420963
Ivan-Navas <ivanrng.work@gmail.com>
2025-09-05 16:53:20 -0500

    feat: :sparkles: get profile with google

--- src/app/api/user/profile/route.ts
+++ src/app/api/user/profile/route.ts
@@ -2,40 +2,68 @@ import { NextRequest, NextResponse } from "next/server";
 import jwt from "jsonwebtoken";
 import { prisma } from "@/libs/prisma";
 import { TokenInterface } from "@/interface/user";
+import { getToken } from "next-auth/jwt";
 
 export const GET = async (req: NextRequest) => {
   try {
     const token = req.cookies.get("token")?.value;
-    if(!token){
+    const tokenGoogle = await getToken({req, secret: process.env.NEXTAUTH_SECRET});
+    
+    if(!token && !tokenGoogle){
       return NextResponse.json({
         status: "error",
         message: "No se ha podido autenticar"
       })
     }
-    const SECRET = process.env.SECRET_VALUE;
-    const verify = jwt.verify(token, SECRET!);
-    const userId = (verify as TokenInterface).id;
-    const userExist = await prisma.user.findUnique({
-      where: {
-        id: userId,
-      },
-      select: {
-        id: true,
-        email: true,
-        name: true,
-      }
-    })
-    if(!userExist){
-      return NextResponse.json({
-        status: "error",
-        message: "El usuario no existe"
+    if(token){
+      const SECRET = process.env.SECRET_VALUE;
+      const verify = jwt.verify(token, SECRET!);
+      const userId = (verify as TokenInterface).id;
+      const userExist = await prisma.user.findUnique({
+        where: {
+          id: userId,
+        },
+        select: {
+          id: true,
+          email: true,
+          name: true,
+        }
       })
+      if(!userExist){
+        return NextResponse.json({
+          status: "error",
+          message: "El usuario no existe"
+        })
+      }
+      return NextResponse.json({
+        status: "success",
+        message: "Perfil obtenido con exito",
+        user: userExist,
+      });
     }
-    return NextResponse.json({
-      status: "success",
-      message: "Perfil obtenido con exito",
-      user: userExist,
-    });
+    if(tokenGoogle){
+      const userGExist = await prisma.user.findFirst({
+        where: {
+          email: tokenGoogle.email!,
+        },
+        select: {
+          id: true,
+          email: true,
+          name: true,
+        }
+      })
+      if(!userGExist){
+        return NextResponse.json({
+          status: "error",
+          message: "El usuario no existe"
+        })
+      }
+      return NextResponse.json({
+        status: "success",
+        message: "Perfil obtenido con exito",
+        user: userGExist,
+      });
+    }
   } catch (error) {
     return NextResponse.json({
       status: "error",
