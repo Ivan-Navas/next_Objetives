6a93f2556b940d114753c887593cdad3c68e6223
Ivan-Navas <ivanrng.work@gmail.com>
2025-09-29 16:08:29 -0500

    fix: :bug: fixed caroucel

--- src/components/ui/CaroucelCard.tsx
+++ src/components/ui/CaroucelCard.tsx
@@ -4,6 +4,7 @@ import React from "react";
 //import { BiChevronRight, BiChevronLeft } from "react-icons/bi";
 import { useAppContext } from "@/Context";
 import { CaroucelObjetive } from "@/interface/caroucel";
+import CircleCaroucel from "./CircleCaroucel";
 //import Image from "next/image";
 
 type Props = {
@@ -33,40 +34,10 @@ const CaroucelCard = (props: Props) => {
                     %
                   </h2>
                   <div className="grid relative items-center margin-0">
-                    <svg width="70" height="70" viewBox="0 0 70 70 ">
-                      <defs>
-                        <linearGradient id="linearS">
-                          <stop offset="0%" stopColor="#fff" stopOpacity="0.2" />
-                          <stop offset="10%" stopColor="#87E5D3" />
-                          <stop offset="50%" stopColor="#BAACDD" />
-                        </linearGradient>
-                      </defs>
-                      <circle
-                        r="30"
-                        cx="35"
-                        cy="35"
-                        pathLength="100"
-                        className="fill-none stroke-gray-700 stroke-[10] flex items-center justify-center"
-                      />
-                      <circle
-                        r="30"
-                        cx="35"
-                        cy="35"
-                        pathLength="100"
-                        className="fill-none stroke-[10] flex items-center justify-center"
-                        strokeLinecap="round"
-                        style={{
-                          strokeDasharray:
-                            props.objetive.amount > 0
-                              ? `${getPorcent(props.objetive.progress, props.objetive.amount)},100`
-                              : "0,100",
-                          transform: "rotate(-90deg)",
-                          transformOrigin: "50% 50%",
-                          stroke: "url(#linearS)",
-                          transition: "stroke-dasharray 0.5s ease-out",
-                        }}
-                      />
-                    </svg>
+                    <CircleCaroucel
+                      progress={props.objetive.progress}
+                      amount={props.objetive.amount}
+                    />
                     <img
                       src={props.objetive.image}
                       alt="objetive_icon"
